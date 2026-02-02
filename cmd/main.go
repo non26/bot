ff6c5115bd773e6c8dfd52dd64517c337faa6b24
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2026-02-02 22:33:52 +0700

    feat: bot by heikin ashi

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"bot/app/bff/bot_trade_management/route"
+	"bot/config"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+	router := gin.Default()
+
+	route.Route(router, config)
+	router.Run(fmt.Sprintf(":%d", config.Port))
+}
